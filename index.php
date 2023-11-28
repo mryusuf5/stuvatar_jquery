bcc1c5a323c35441639d1ee9147806d35bb7435e
yusuf <yusufyildiz@live.nl>
2023-11-28 13:28:22 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet/style.css">
+    <script src="https://unpkg.com/htmx.org@1.9.8"></script>
+    <title>stuvatar</title>
+</head>
+<body>
+    
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="scripts/script.js"></script>
+</body>
+</html>
+                
